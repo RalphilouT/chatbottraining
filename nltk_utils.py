9118e3e50f754f0bdb6e21b49e8a9c267df578e7
Ralphilou Tatoy <ralphilou24@gmail.com>
2024-07-09 15:18:51 -0700

    part 3

--- nltk_utils.py
+++ nltk_utils.py
@@ -19,12 +19,12 @@ def bag_of_words(tokenized_sentence, all_words):
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
     bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
-    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    sentence_words = [stem(w) for w in tokenized_sentence]
     bag = np.zeros(len(all_words), dtype= np.float32)
     # Get index and word from all_word
-    for idx, w in enumerate(all_words):
+    for idx, word in enumerate(all_words):
         # if tokenize sentence is in words, then acknowledge
-        if w in tokenized_sentence:
+        if word in sentence_words:
             bag[idx] = 1.0
 
     return bag
--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from model import Net
+
 with open('intents.json','r') as f:
     intents = json.load(f)
 
@@ -24,7 +26,7 @@ for intent in intents['intents']:
         every_word.extend(w)
         xy.append((w, tag))
 
-disregard_w = ['?', "!", ".", ","]
+disregard_w = ['?', "!", "."]
 every_word = [stem(w) for w in every_word if w not in disregard_w]
 every_word = sorted(set(every_word))
 tags = sorted(set(tags))
@@ -34,7 +36,7 @@ ytag_train = []
 
 for(pattern_sentence, tag) in xy:
     bag = bag_of_words(pattern_sentence, every_word)
-    xword_train.append(tag)
+    xword_train.append(bag)
 
     # Numbers for labels
     label = tags.index(tag)
@@ -55,7 +57,59 @@ class ChatBotDataset(Dataset):
     def __len__(self):
         return self.n_samples
 
+# Parameters
 batch_size = 8
+# layer 
+hidden_size = 8
+# Number of tags/class 
+output_size = len(tags)
+# Length of bag of words or every_word 
+input_size = len(xword_train[0])
+learning_rate = 0.001
+num_epochs = 1000
+
 dataset = ChatBotDataset()
 train_dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net(input_size, hidden_size, output_size).to(device)
+
+#loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training loop 
+for epoch in range(num_epochs):
+    for(words, labels) in train_dataloader:
+        words = words.to(device)
+        # labels = labels.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+
+        # forward pass 
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        # backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
     
+    if(epoch +1) % 100 == 0:
+        print(f'epoch {epoch +1}/{num_epochs}, loss ={loss.item():.4f}')
+
+print(f'final loss, loss ={loss.item():.4f}')
+
+# Save data 
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "every_words": every_word,
+    "tags" : tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. saved to {FILE}')
